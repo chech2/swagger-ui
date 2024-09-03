466d370f9a0e10d4ebce9cdab7c7cdded9797cfa
Leechaelim <chaelim3396@gmail.com>
2024-09-03 23:48:42 +0900

    Update swagger-initializer.js
    경로 수정

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://github.com/chech2/swagger-ui/blob/master/SAMPLE%20-%200.0.1.json",
+    url: "https://raw.githubusercontent.com/chech2/swagger-ui/master/SAMPLE%20-%200.0.1.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
